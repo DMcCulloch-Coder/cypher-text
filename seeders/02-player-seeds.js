1f96f6b965f2e7bdc796a25eb89d20ac27f4ea4e
stevevillardi <stevevillardi@gmail.com>
2020-01-09 02:31:41 -0500

    updated seed files

--- seeders/02-player-seeds.js
+++ seeders/02-player-seeds.js
@@ -5,72 +5,72 @@ module.exports = {
 			[
 				{
 					player_name: "Steve",
-					player_team: 0,
-					player_type: 3,
+					player_team: 1,
+					player_type: 0,
 					room_id: 1,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "David",
-					player_team: 0,
-					player_type: 3,
+					player_team: 2,
+					player_type: 0,
 					room_id: 1,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "Dana",
-					player_team: 0,
-					player_type: 3,
+					player_team: 3,
+					player_type: 1,
 					room_id: 1,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "Steve",
-					player_team: 0,
-					player_type: 3,
+					player_team: 1,
+					player_type: 0,
 					room_id: 2,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "David",
-					player_team: 0,
-					player_type: 3,
+					player_team: 2,
+					player_type: 0,
 					room_id: 2,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "Dana",
-					player_team: 0,
-					player_type: 3,
+					player_team: 3,
+					player_type: 1,
 					room_id: 2,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "Steve",
-					player_team: 0,
-					player_type: 3,
+					player_team: 1,
+					player_type: 0,
 					room_id: 3,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "David",
-					player_team: 0,
-					player_type: 3,
+					player_team: 2,
+					player_type: 0,
 					room_id: 3,
 					created_at: new Date(),
 					updated_at: new Date()
 				},
 				{
 					player_name: "Dana",
-					player_team: 0,
-					player_type: 3,
+					player_team: 3,
+					player_type: 1,
 					room_id: 3,
 					created_at: new Date(),
 					updated_at: new Date()
