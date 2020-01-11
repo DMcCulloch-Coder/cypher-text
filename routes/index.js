7633f94adf1a2ba371558addfe96de8297ea4b94
David <DMcCulloch86@gmail.com>
2020-01-11 10:39:03 -0500

    merging

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //index.js will server as the html routes while the *-api will be the model specific api routes
 router.get("/", (req, res, next) => {
-	res.render("index", { title: "Cypher-Text" });
+    res.render("index", { title: "Cypher-Text" });
 });
 
 module.exports = router;
