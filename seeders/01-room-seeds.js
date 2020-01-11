f9de34479f6e12920bbf24517d8e6e8803727c3f
David <DMcCulloch86@gmail.com>
2020-01-11 10:01:05 -0500

    eslint config

--- seeders/01-room-seeds.js
+++ seeders/01-room-seeds.js
@@ -1,35 +1,35 @@
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.bulkInsert(
-			"Rooms",
-			[
-				{
-					room_name: "Example Room 1",
-					track_history: 0,
-					team_count: 3,
-					created_at: new Date(),
-					updated_at: new Date()
-				},
-				{
-					room_name: "Example Room 2",
-					track_history: 0,
-					team_count: 3,
-					created_at: new Date(),
-					updated_at: new Date()
-				},
-				{
-					room_name: "Example Room 3",
-					track_history: 0,
-					team_count: 3,
-					created_at: new Date(),
-					updated_at: new Date()
-				}
-			],
-			{}
-		);
-	},
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Rooms",
+            [
+                {
+                    room_name: "Example Room 1",
+                    track_history: 0,
+                    team_count: 3,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                },
+                {
+                    room_name: "Example Room 2",
+                    track_history: 0,
+                    team_count: 3,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                },
+                {
+                    room_name: "Example Room 3",
+                    track_history: 0,
+                    team_count: 3,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                }
+            ],
+            {}
+        );
+    },
 
-	down: (queryInterface, Sequelize) => {
+    down: (queryInterface, Sequelize) => {
 		/*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -37,5 +37,5 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-	}
+    }
 };
