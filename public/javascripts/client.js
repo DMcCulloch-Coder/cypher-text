659b7cece93d9151eeed1d3fdcef88584dec22de
Dana Cronin <cronin43224235@gmail.com>
2020-01-10 19:17:28 -0500

    route theories

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,16 +3,22 @@
 //------ Inputs -----
 
 // new or returning user
+    //"/api/players/:name?"
 
 // new or joining room
+    //"api/rooms/:name?"
 
 // team choices
+    //"api/rooms/:id"
 
 // keymaster choices
+    //"api/rooms/:id/players/:id?"
 
 // submit clue
+    //"api/rooms/:id/pastclues/:team"
 
 // submit guess
+    //api/rooms/:id/words/:id
 
 // ----- Find (^nest?) -------
 
@@ -23,9 +29,13 @@
 // words (render and on click functonality)
 
 // teams' players for lists
+    //"api/rooms/:id/players"
 
 // past clues
+    //"api/rooms/:id/pastclues"
 
 // player_type
+    // api/rooms/:id/players /?/
 
 // scores = not-visible.length?
+    //"api/rooms/:id/words"
