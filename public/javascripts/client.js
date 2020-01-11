788dfd35427513b6c298468f9e6767245e6c6c3c
Dana Cronin <cronin43224235@gmail.com>
2020-01-10 19:09:01 -0500

    some inputs vs pull needs?

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,31 @@
 //place holder for ajax api requests
+
+//------ Inputs -----
+
+// new or returning user
+
+// new or joining room
+
+// team choices
+
+// keymaster choices
+
+// submit clue
+
+// submit guess
+
+// ----- Find (^nest?) -------
+
+// (^if) returning user
+
+// (^if) room does not exist reroute to create room
+
+// words (render and on click functonality)
+
+// teams' players for lists
+
+// past clues
+
+// player_type
+
+// scores = not-visible.length?
