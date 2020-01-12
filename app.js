ff68919696b95dbe49042a87855d3a6ca8426aa0
Dana Cronin <cronin43224235@gmail.com>
2020-01-12 17:22:33 -0500

    fixing partial path

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "/views/partials"));
+app.set("views", path.join(__dirname, "/views"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
