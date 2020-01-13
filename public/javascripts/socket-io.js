2b7e1266e2fd1146e3ee5ec0bb7e5dd48a0fc2f6
stevevillardi <stevevillardi@gmail.com>
2020-01-13 09:22:32 -0500

    updated socket.io config

--- public/javascripts/socket-io.js
+++ public/javascripts/socket-io.js
@@ -1,6 +1,23 @@
 const socket = io();
 let randomNumber = Math.random();
-socket.emit("hello-world", randomNumber);
-socket.on("hello-world", data => {
-    console.log(`User sent the number: ${data}`);
+
+//Generates 5 character string for room id
+function generateRoomID() {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+    let string_length = 5;
+    let id = "";
+    for (let i = 0; i < string_length; i++) {
+        let num = Math.floor(Math.random() * chars.length);
+        id += chars.substring(num, num + 1);
+    }
+    return id;
+}
+
+//generates room ID and instructs client to join that room ID
+let roomID = generateRoomID();
+socket.emit("broadcast-room", roomID);
+socket.on("joined-room", data => {
+    console.log(`You have joined room ID: ${data}`);
 });
+
+//Once client initially connects we allow it to create a room and send that back to the server to setup
